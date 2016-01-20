1436644f500d0443ba274008abe1b239cd1ffc38
Eeliya Rasta <eeliya.rasta@gmail.com>
2016-01-20 15:45:05 +0100

    - Bug fixes and improvements

--- packages/admin/public/js/ew-tags/ew-tags.js
+++ packages/admin/public/js/ew-tags/ew-tags.js
@@ -42,124 +42,130 @@
 })();
 
 (function (xtag) {
+   /*function builder(elementClass) {
 
-   var EW_List = {
-      _element: null,
-      data: [],
-      value: null,
-      render: function (data) {
-         //var data = this.data;
-         this.innerHTML = "";
-         var a = null;
-         for (var i = 0, len = data.length; i < len; i++) {
-            data[i]._itemIndex = i;
-            var item = xtag.createFragment(UIUtil.populate(this.template, data[i]));
-            a = xtag.query(item, "a")[0];
+   }*/
 
-            if (data[i].id)
-               this.links[data[i].id] = a;
+   var EWListBuilder = function () {
+      var Root = {
+         _element: null,
+         data: [],
+         value: null,
+         render: function (data) {
+            //var data = this.data;
+            this.innerHTML = "";
+            var a = null;
+            for (var i = 0, len = data.length; i < len; i++) {
+               data[i]._itemIndex = i;
+               var item = xtag.createFragment(UIUtil.populate(this.template, data[i]));
+               a = xtag.query(item, "a")[0];
 
-            this.links[i] = a;
-            a.dataset.index = i;
+               if (data[i].id)
+                  this.links[data[i].id] = a;
 
-            this.appendChild(item);
-         }
-      },
-      selectItem: function (i, element) {
-         if (EW_List.methods.onItemSelected) {
-            EW_List.methods.onItemSelected(EW_List._element.data[i], i, element);
-         }
+               this.links[i] = a;
+               a.dataset.index = i;
 
-         /*if (EW_List.selectedElement) {
-          UIUtil.removeCSSClass(EW_List.selectedElement, "selected");
-          }
-          
-          EW_List.selectedElement = element;
-          UIUtil.addCSSClass(EW_List.selectedElement, "selected");*/
-      }
-   };
-   EW_List.lifecycle = {
-      created: function () {
-         EW_List._element = this;
-         this.template = this.innerHTML;
-         this.innerHTML = "";
-         this.links = {};
-         this.data = [];
-         this.value = null;
-      },
-      inserted: function () {
-      },
-      removed: function () {
-      },
-      attributeChanged: function (attrName, oldValue, newValue) {
-      }
-   };
-   EW_List.methods = {
-      onSetData: function (data) {
-      },
-      onItemSelected: function (item, i, element) {
-      }
-   };
-
-   EW_List.accessors = {
-      data: {
-         attribute: {},
-         set: function (value) {
-
-            EW_List.value = null;
-            if ("object" !== typeof value) {
-               EW_List.data = [];
-               value = [];
-               //return;
+               this.appendChild(item);
             }
-
-            EW_List.data = value;
-
-            if (EW_List.methods.onSetData) {
-               EW_List.methods.onSetData(value);
-            }
-
-            EW_List.render.call(this, value);
          },
-         get: function () {
-            return EW_List.data;
-         }
-      },
-      onItemSelected: {
-         attribute: {},
-         set: function (value) {
-            EW_List.methods.onItemSelected = value;
-         }
-      },
-      onSetData: {
-         attribute: {},
-         set: function (value) {
-            EW_List.methods.onSetData = value;
-         }
-      },
-      value: {
-         attribute: {},
-         set: function (value) {
-            value = parseInt(value);
-
-            if (value > -1 /*&& value !== this.value*/ && EW_List.data.length) {
-               EW_List.selectItem(value, this.links[value]);
+         selectItem: function (i, element) {
+            if (Root.methods.onItemSelected) {
+               Root.methods.onItemSelected(Root._element.data[i], i, element);
             }
 
-            EW_List.value = value;
-         },
-         get: function () {
-            return EW_List.value;
+            /*if (EW_List.selectedElement) {
+             UIUtil.removeCSSClass(EW_List.selectedElement, "selected");
+             }
+             
+             EW_List.selectedElement = element;
+             UIUtil.addCSSClass(EW_List.selectedElement, "selected");*/
          }
-      }
+      };
+      Root.lifecycle = {
+         created: function () {
+            Root._element = this;
+            this.template = this.innerHTML;
+            this.innerHTML = "";
+            this.links = {};
+            this.data = [];
+            this.value = null;
+         },
+         inserted: function () {
+         },
+         removed: function () {
+         },
+         attributeChanged: function (attrName, oldValue, newValue) {
+         }
+      };
+      Root.methods = {
+         onSetData: function (data) {
+         },
+         onItemSelected: function (item, i, element) {
+         }
+      };
+
+      Root.accessors = {
+         data: {
+            attribute: {},
+            set: function (value) {
+
+               Root.value = null;
+               if ("object" !== typeof value) {
+                  Root.data = [];
+                  value = [];
+                  //return;
+               }
+
+               Root.data = value;
+
+               if (Root.methods.onSetData) {
+                  Root.methods.onSetData(value);
+               }
+
+               Root.render.call(this, value);
+            },
+            get: function () {
+               return Root.data;
+            }
+         },
+         onItemSelected: {
+            attribute: {},
+            set: function (value) {
+               Root.methods.onItemSelected = value;
+            }
+         },
+         onSetData: {
+            attribute: {},
+            set: function (value) {
+               Root.methods.onSetData = value;
+            }
+         },
+         value: {
+            attribute: {},
+            set: function (value) {
+               value = parseInt(value);
+
+               if (value > -1 /*&& value !== this.value*/ && Root.data.length) {
+                  Root.selectItem(value, this.links[value]);
+               }
+
+               Root.value = value;
+            },
+            get: function () {
+               return Root.value;
+            }
+         }
+      };
+
+      Root.events = {
+         "click:delegate(a)": function (e) {
+            e.preventDefault();
+            Root._element.value = e.target.dataset.index;
+         }
+      };
+      return Root;
    };
 
-   EW_List.events = {
-      "click:delegate(a)": function (e) {
-         e.preventDefault();
-         EW_List._element.value = e.target.dataset.index;
-      }
-   };
-
-   xtag.register("ew-list", EW_List);
+   xtag.register("ew-list", EWListBuilder());
 })(xtag);
