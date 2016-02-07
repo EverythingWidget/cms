c7a77ba005ac5d232c3ffe4e5704f903a71a57b6
Eeliya <eeliya.rasta@gmail.com>
2016-02-07 16:14:13 +0100

    - Added: ew-sortable-list

--- packages/admin/public/js/lib/system-tags.js
+++ packages/admin/public/js/lib/system-tags.js
@@ -339,7 +339,7 @@
       inserted: function () {
         if (this.validate)
           return;
-        
+
         if (!System.uiTemplates["system/" + this.module]) {
           System.uiTemplates["system/" + this.module] = {};
         }
@@ -371,5 +371,199 @@
   };
 
   xtag.register("system-ui-view", ewUITemplate);
+
+  var sortableList = {
+    lifecycle: {
+      created: function () {
+        this.xtag.placeHolder = document.createElement("li");
+        this.xtag.placeHolder.className += "placeholder";
+        this.style.overflow = "hidden";
+        this.isValidParent = function () {
+          return true;
+        };
+        this.onDrop = function () {
+        };
+      },
+      inserted: function () {
+
+      },
+      removed: function () {
+
+      }
+    },
+    events: {
+      mousedown: function (event) {
+        //console.log("down");
+      },
+      "mousedown:delegate(.handle)": function (e) {
+        var dim = this.getBoundingClientRect();
+        e.currentTarget.xtag.initDragPosition = {
+          x: e.pageX - dim.left,
+          y: e.pageY - dim.top
+        };
+
+        var draggedItem = this;
+        while (draggedItem.tagName.toLowerCase() !== "li") {
+          draggedItem = draggedItem.parentNode;
+        }
+
+        var diDimension = draggedItem.getBoundingClientRect();
+        e.currentTarget.xtag.draggedItem = draggedItem;
+        draggedItem.style.position = "fixed";
+        draggedItem.style.width = diDimension.width + "px";
+        draggedItem.style.height = diDimension.height + "px";
+
+        //console.log(e, draggedItem);
+        e.stopPropagation();
+        e.preventDefault();
+      },
+      "mouseup:delegate(.handle)": function (e) {
+        e.stopPropagation();
+        e.preventDefault();
+      },
+      mousemove: function (event) {
+        if (!this.xtag.draggedItem)
+          return;
+
+        var groups = this.querySelectorAll("ul");
+        var groupDim = [];
+        for (var i = 0, len = groups.length; i < len; i++) {
+          groupDim.push(groups[i].getBoundingClientRect());
+        }
+
+        var parent = null;
+        var index = 0;
+        var indexElement = null;
+
+        for (var i = groupDim.length - 1; i >= 0; i--) {
+          var parentDim = groupDim[i];
+          if (event.pageX > parentDim.left && event.pageX < parentDim.right && event.pageY > parentDim.top && event.pageY < parentDim.bottom) {
+            parent = groups[i];
+            //indexElement = parent.lastChild;
+            var children = parent.childNodes || [];
+            var childElements = [];
+            //index = childElements.length;
+            for (var n = 0; n < children.length; n++) {
+              if (children[n].tagName.toLowerCase() !== "li" || children[n] === this.xtag.draggedItem /*|| children[n].className === "placeholder"*/)
+                continue;
+              childElements.push(children[n]);
+            }
+            //console.log(childElements)
+            var extra = {
+              height: 0,
+              left: 0
+            };
+            for (n = childElements.length - 1; n >= 0; n--) {
+              if (childElements[n].className === "placeholder") {
+                //extra = childElements[n].getBoundingClientRect();
+                //console.log(extra.height)
+                continue;
+              }
+
+              var childDim = childElements[n].getBoundingClientRect();
+
+              if (event.pageY > childDim.top && event.pageY < childDim.top + (childDim.height / 2) /*&& event.pageY + extra.height < childDim.bottom - (childDim.height / 2)*/) {
+                index = n;
+                indexElement = childElements[index] /*|| parent.firstChild*/;
+                //console.log("above", index);
+                //console.log(childDim, event.pageY, n)
+                break;
+              } else if (event.pageY >= childDim.top + (childDim.height / 2) /*&& event.pageY < childDim.bottom*/) {
+                index = n;
+                indexElement = childElements[index].nextSibling;
+                //console.log("lower", index);
+                //console.log(childDim, event.pageY, n)
+                break;
+              } else {
+                indexElement = this.xtag.tempIndexElement;
+                //console.log(extra, event.pageY)
+              }
+              //console.log(extra)
+              //extra.height = 0;
+              //extra.top = 0;
+            }
+            break;
+          }
+        }
+
+        this.xtag.draggedItem.style.left = event.pageX + 1 + "px";
+        this.xtag.draggedItem.style.top = event.pageY + 1 + "px";
+
+        if (parent && (this.xtag.tempParent !== parent || this.xtag.tempIndexElement !== indexElement)) {
+          this.xtag.tempParent = parent;
+          this.xtag.tempIndex = index;
+          this.xtag.tempIndexElement = indexElement;
+          if (this.isValidParent(this.xtag.draggedItem, parent, this.xtag.tempIndex)) {
+            //console.log(indexElement)
+            if (indexElement && indexElement.parentNode === parent)
+              parent.insertBefore(this.xtag.placeHolder, indexElement);
+            else if (!indexElement)
+              parent.insertBefore(this.xtag.placeHolder, indexElement);
+          }
+        }
+      },
+      mouseup: function (event) {
+        //console.log("up");
+        if (this.xtag.draggedItem) {
+          this.xtag.draggedItem.style.position = "";
+          this.xtag.draggedItem.style.width = "";
+          this.xtag.draggedItem.style.height = "";
+          this.xtag.draggedItem.style.left = "";
+          this.xtag.draggedItem.style.top = "";
+
+          if (this.xtag.placeHolder.parentNode) {
+            this.onDrop(this.xtag.draggedItem, this.xtag.tempParent, this.xtag.tempIndex);
+            this.xtag.placeHolder.parentNode.replaceChild(this.xtag.draggedItem, this.xtag.placeHolder);
+          }
+
+          this.xtag.draggedItem = null;
+          this.xtag.tempParent = null;
+          this.xtag.tempIndex = null;
+        }
+      }
+    }
+  };
+
+  xtag.register("system-sortable-list", sortableList);
+
+
+  var inputNumber = {
+    lifecycle: {
+      created: function () {
+        this.xtag.input = document.createElement("input");
+        this.xtag.input.value = this.getAttribute("value");
+        this.tabIndex = 1;
+      },
+      inserted: function () {
+        this.appendChild(this.xtag.input);
+      },
+      removed: function () {
+        
+      }
+    },
+    accessors: {
+      value: {
+        attribute: {},
+        set: function (value) {
+          this.xtag.value = value;
+        },
+        get: function () {
+          return this.xtag.value;
+        }
+      }
+    },
+    methods: {
+      increase: function () {
+        
+      }
+    },
+    events: {
+      "focus:delegate(input)": function (event) {
+        event.currentTarget.focus();
+      }
+    }
+  };
+
+  xtag.register("system-input-number", inputNumber);
 
 })(xtag, System);
